a141432886bb7af5296b993c40d5ff3f78aa0b0c
clearine <32025159+clearine@users.noreply.github.com>
2020-07-02 06:12:05 -0230

    ...for real this time.

--- lib/cls.dart
+++ lib/cls.dart
@@ -1,19 +1,4 @@
 import 'dart:io';
-import 'package:args/args.dart';
-
-ArgResults argResults;
-
-void main(List<String> args) {
-  final parser = ArgParser()..addFlag('recursive', negatable: false, abbr: 'r');
-
-  argResults = parser.parse(args);
-
-  if (argResults.rest.isEmpty) {
-    cls(Directory.current, argResults['recursive']);
-  } else {
-    cls(Directory(argResults.rest[0].toString()), argResults['recursive']);
-  }
-}
 
 void cls(Directory folder, bool recursive) {
   if (recursive) {
